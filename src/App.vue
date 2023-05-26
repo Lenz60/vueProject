657ebd06c9664fc6f88a42312d68b71804e73bd9
Rafly Andrian <raflyandrian@gmail.com>
2023-05-27 00:13:15 +0700

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div class="flex flex-col justify-center items-center">
+    <div>
+      <a href="https://vitejs.dev" target="_blank">
+        <img src="/vite.svg" class="logo" alt="Vite logo" />
+      </a>
+      <a href="https://vuejs.org/" target="_blank">
+        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      </a>
+    </div>
+    <div class="pl-5">
+      <p>Значимость этих проблем настолько очевидна, что курс на социально-ориентированный национальный проект требует определения и уточнения прогресса профессионального общества.</p>
+    </div>
+    <HelloWorld msg="Vite + Vue" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="flex-col justify-center">
+    <h1>{{ msg }}</h1>
+  
+    <div class="flex-1">
+      <button class= "bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" type="button" @click="count++">count is {{ count }}</button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+    </div>
+
+    <div class="flex-1">
+      <p class="text-red-500 text-lg">
+        Check out
+        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
+          >create-vue</a
+        >, the official Vue + Vite starter
+      </p>
+    </div>
+    
+    <div class="flex-1">
+      <p>
+        Install
+        <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
+        in your IDE for a better DX
+      </p>
+      <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    </div>
+
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
