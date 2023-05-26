7ffe0599d1c39459fc4a752ef6bd300839f9d9cf
Rafly Andrian <raflyandrian@gmail.com>
2023-05-27 01:33:01 +0700

    Atlas Landingpage Template

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,14 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+module.exports = {
+  plugins: [
+    require("tailwindcss/nesting"),
+    require("tailwindcss"),
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          require("cssnano")({
+            preset: "default",
+          }),
+        ]
+      : []),
+  ],
+};
